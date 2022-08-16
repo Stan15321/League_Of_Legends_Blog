2aa5fb6b503d833eeb0147bf954e06256c7dbe81
Stanislav Nikolov <stanislavnikolov567@gmail.com>
2022-08-16 16:22:44 +0300

    Login/logout/ImageUpload/AdminPanel/Styling

--- Blog1/Models/Post.cs
+++ Blog1/Models/Post.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 
 namespace Blog1.Models
@@ -6,9 +11,12 @@ namespace Blog1.Models
     public class Post
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Body { get; set; }
+        public string Title { get; set; } = "";
+        public string Body { get; set; } = "";
+        public string Image { get; set; } = "";
 
         public DateTime Created { get; set; } = DateTime.Now;
+
+        
     }
 }
